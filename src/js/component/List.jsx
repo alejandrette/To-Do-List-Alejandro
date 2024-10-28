acf62bb02e5c83148d8215eaf079d28805173bbc
Alejandro Casasola <120450320+alejandrette@users.noreply.github.com>
2024-10-28 20:06:37 +0000

    create list

---
+++ src/js/component/List.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+const List = () => {
+    const [inputValue, setInputValue] = useState('');
+    const [listTask, setListTask] = useState([]);
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    }
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            setListTask(prev => prev.concat(inputValue));
+            setInputValue(e.target.value = '');
+        }
+    }
+	
+    const hiddenMenssageError = () => document.querySelector('.message-error').classList.add('visually-hidden');
+    
+    return (
+		<div className='bg-info bg-opacity-10 border border-info rounded p-5'>
+            <div>
+            <h1 className="text-center">Task</h1>
+                <input
+                    type="text"
+                    id="task"
+                    className="form-control border-light bg-transparent"
+                    onChange={handleInputChange}
+                    onKeyDown={handleKeyDown}
+                    onClick={hiddenMenssageError}
+                    placeholder="Add a New Task"
+                />
+            </div>
+            <div className='message-error visually-hidden'>Enter some task</div>
+            <div className="show-task mt-4">
+                <ul className="list-task">
+                    {listTask.map((task, index)=> (
+                        <li key={index} className="task-item">
+                            <span>🍃</span> {task} {index}
+                            <button className="btn btn-danger" onClick={() => {
+                                const newArr = listTask.filter(taskOut => taskOut != task);
+                                setListTask(newArr);
+                            }}>X</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+		</div>
+	);
+};
+
+export default List;
